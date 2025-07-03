12866b2e64660e2f47b4597501b799c6f4ec01a1
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-03 23:21:17 +0500

    Add frontend\src\pages\ProductList.js

---
+++ frontend\src\pages\ProductList.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/products')
+      .then(response => {
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product._id}>
+            <Link to={`/products/${product._id}`}>{product.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
